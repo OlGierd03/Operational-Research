d1701581f9d230039dc90e39b24d113bb67798e3
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-04-06 22:44:51 +0200

    Update main() for Simuation

--- Operational-Research/src/main/java/agh/or/Main.java
+++ Operational-Research/src/main/java/agh/or/Main.java
@@ -2,9 +2,15 @@ package agh.or;
 
 public class Main {
     public static void main(String[] args) {
-        for (var invalidLight : LightsSets.getValids()){
-            System.out.println(invalidLight);
-        }
-        System.out.println(LightsSets.getValids().size());
+        Configuration configuration = new Configuration(
+                Lights.LIGHT_COUNT * 5,
+                10,
+                15,
+                30,
+                100
+        );
+
+        Simulation simulation = new Simulation(configuration, Solution.random(configuration));
+        simulation.run(true);
     }
 }
