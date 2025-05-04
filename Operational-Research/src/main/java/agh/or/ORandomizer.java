2edd494c209ec58e0d5594efca959e7e90a3726b
Dominik Dróżdż <ddrozdz@student.agh.edu.pl>
2025-05-05 00:56:35 +0200

    Added seeding to ORandomizer

--- Operational-Research/src/main/java/agh/or/ORandomizer.java
+++ Operational-Research/src/main/java/agh/or/ORandomizer.java
@@ -5,10 +5,15 @@ import java.util.List;
 import java.util.Random;
 
 public class ORandomizer {
+    private static Random random = new Random();
+
+    public static void setSeed(long seed) {
+        random = new Random(seed);
+    }
+
     public static List<O> randomize(Configuration configuration) {
         List<O> result = new ArrayList<O>();
         var validSet = LightsSets.getValids();
-        var random = new Random();
 
         while (!Solution.willEnd(result)){
             var lightsToAdd = validSet.stream()
