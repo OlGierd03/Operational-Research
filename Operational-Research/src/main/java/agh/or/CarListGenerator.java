5865cfabda41bd3f5999588cccc8c58009d78fb5
Dominik Dróżdż <ddrozdz@student.agh.edu.pl>
2025-05-05 03:00:48 +0200

    Added comments

--- Operational-Research/src/main/java/agh/or/CarListGenerator.java
+++ Operational-Research/src/main/java/agh/or/CarListGenerator.java
@@ -6,15 +6,15 @@ import java.util.Random;
 
 // Propozycja żeby zmienić nazwę klasy na CarFactory
 public class CarListGenerator {
-    public static List<Integer> createCars(Configuration configuration) {
+    public static List<Integer> createCars(Configuration configuration) { // funkcja zwraca liczbę samochodów na każdym pasie
         List<Integer> carList = new ArrayList<>();
-        for (int i = 0; i < Lights.LIGHT_COUNT; i++) {
+        for (int i = 0; i < Lights.LIGHT_COUNT; i++) { // Tworzymy pozycje dla każdego pasa na skrzyżowaniu
             carList.add(0);
         }
 
         Random random = new Random();
 
-        for (int i = 0; i < configuration.carCount(); i++) {
+        for (int i = 0; i < configuration.carCount(); i++) { // Dodajemy samochody do losowych pozycji
             int place = random.nextInt(Lights.LIGHT_COUNT);
             carList.set(place, carList.get(place) + 1);
         }
@@ -22,7 +22,7 @@ public class CarListGenerator {
         return carList;
     }
 
-    public static List<Integer> createCars(Configuration configuration, long seed) {
+    public static List<Integer> createCars(Configuration configuration, long seed) { // to samo co wyżej, ale z seedem
         List<Integer> carList = new ArrayList<>();
         for (int i = 0; i < Lights.LIGHT_COUNT; i++) {
             carList.add(0);
